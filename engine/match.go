8a8948ee71e3b1070a145e4a6f98424bc942d202
liaoran123 <2065551801@qq.com>
2026-01-03 15:36:10 +0800

    1

--- engine/match.go
+++ engine/match.go
@@ -1,5 +1,7 @@
 package engine
 
+import "fmt"
+
 // 根据TableIter回传的key,value所有能得到的field的值，进行所需匹配
 type Match interface {
 	//fields *map[string]any接收迭代器传回的值进行匹配
@@ -7,9 +9,11 @@ type Match interface {
 }
 
 type AND struct {
-	//data是TableIter生成的键值map，作为交集，并集，差集等。
+	//需要匹配的字段名，与fields *map[string]any中的key对应
+	fields []string
+	//data是TableIter生成的键值map，作为存在或不存在的判断。
 	data map[any]bool
-	//Rule是判断，true为对应sql语句的 IN 或 AND ，false为NOT IN 或 OR。
+	//rule是判断，true为对应sql语句的 IN 或 AND ，false为NOT IN 或 OR。
 	rule bool
 }
 
@@ -19,7 +23,25 @@ func ANDNew(data map[any]bool, rule bool) *AND {
 		rule: rule,
 	}
 }
+
+//由于data map[any]bool,any只能是一个值，所以规定，fields *map[string]any大于1,则需要将fields中的合并值转换为字符串，再进行匹配
+func (b *AND) mergeFields(fields *map[string]any) (r any) {
+	if len(b.fields) > 1 {
+		r = ""
+		for _, f := range b.fields {
+			r = r.(string) + fmt.Sprintf("%v", (*fields)[f])
+		}
+	} else {
+		r = (*fields)[b.fields[0]]
+	}
+	return r
+}
 func (b *AND) Match(fields *map[string]any) bool {
-	//v := b.valueParse(table, value)
-	return false
+	if len(b.fields) == 0 {
+		return false == b.rule
+	}
+	values := b.mergeFields(fields)
+	_, ok := b.data[values]
+	//rule是判断，true为对应sql语句的 IN 或 AND ，false为NOT IN 或 OR。
+	return ok == b.rule
 }
