502a0465fe88afb04d90e8cebe45f9166041dc56
liaoran123 <2065551801@qq.com>
2026-01-03 17:46:42 +0800

    完成Match功能

--- engine/match.go
+++ engine/match.go
@@ -12,6 +12,7 @@ type AND struct {
 	//需要匹配的字段名，与fields *map[string]any中的key对应
 	fields []string
 	//data是TableIter生成的键值map，作为存在或不存在的判断。
+	//data 是由(t *TableIter) Map(fields ...string) (data map[any]bool)生成
 	data map[any]bool
 	//rule是判断，true为对应sql语句的 IN 或 AND ，false为NOT IN 或 OR。
 	rule bool
@@ -24,12 +25,13 @@ func ANDNew(data map[any]bool, rule bool) *AND {
 	}
 }
 
-//由于data map[any]bool,any只能是一个值，所以规定，fields *map[string]any大于1,则需要将fields中的合并值转换为字符串，再进行匹配
+//由于data map[any]bool,any只能是一个值，所以规定，fields 大于1,则需要将fields *map[string]any中的合并值转换为字符串，再进行匹配
 func (b *AND) mergeFields(fields *map[string]any) (r any) {
 	if len(b.fields) > 1 {
 		r = ""
 		for _, f := range b.fields {
-			r = r.(string) + fmt.Sprintf("%v", (*fields)[f])
+			//用分隔符SPLIT将fields中的值合并为一个字符串
+			r = r.(string) + fmt.Sprintf("%v", (*fields)[f]) + SPLIT
 		}
 	} else {
 		r = (*fields)[b.fields[0]]
@@ -37,11 +39,11 @@ func (b *AND) mergeFields(fields *map[string]any) (r any) {
 	return r
 }
 func (b *AND) Match(fields *map[string]any) bool {
-	if len(b.fields) == 0 {
+	if fields == nil || len(*fields) == 0 {
 		return false == b.rule
 	}
-	values := b.mergeFields(fields)
-	_, ok := b.data[values]
+	value := b.mergeFields(fields)
+	_, ok := b.data[value]
 	//rule是判断，true为对应sql语句的 IN 或 AND ，false为NOT IN 或 OR。
 	return ok == b.rule
 }
--- engine/tableiter.go
+++ engine/tableiter.go
@@ -74,21 +74,6 @@ func (t *TableIter) PrimaryToStr(k []byte) string {
 	return rstr[:len(rstr)-1]
 }
 */
-// 判断是否存在指定的主键记录
-func (t *TableIter) Exist() bool {
-	return t.iter.First()
-}
-
-// 统计索引记录数
-func (t *TableIter) Count() int {
-	t.mu.Lock()
-	defer t.mu.Unlock()
-	i := 0
-	for t.iter.Next() {
-		i++
-	}
-	return i
-}
 
 // 分页变量
 type Page struct {
@@ -116,7 +101,7 @@ func PageNew(No ...int) Page {
 // 通过二级索引解析索引记录
 // 组合主键需要反转义
 // 组合主键时，所有索引对应的主键值都是经过转义的。
-// 这是为了提取主键中的某个字段值作为匹配索引，必须转义才能正确根据转义符分割提取。
+// 这是为了提取组合主键中的某个字段值作为匹配索引的功能，必须转义才能正确根据转义符分割提取。
 // 所以在用索引的主键值回表记录时，需要对索引值进行反转义。
 func (t *TableIter) ByIndexGetRecord(v []byte) record.Record {
 	value := v
@@ -153,11 +138,12 @@ func (t *TableIter) ByPrimaryGetRecord(v []byte) record.Record {
 
 // 还原全文索引的value原始值，专用于全文索引。
 // 全文索引设计索引时，规定必须在末尾将主键全量追加到key的键值中。为了避免重复储存字段值，设置value键值为空。
-func (t *TableIter) RestoreValByIndex(k []byte) (Value []byte) {
+func (t *TableIter) RestoreValByFullTextIndex(k []byte) (Value []byte) {
 	fieldsBytes := t.GetFullTextFieldValues(k)
 	Value = t.table.indexs.GetPrimaryKey().GetID(fieldsBytes)
 	return
 }
+
 func (t *TableIter) GetFullTextFieldValues(k []byte) *map[string][]byte {
 	ks := util.Bytes(k).Split() //分解并反转义
 	pkfields := t.table.indexs.GetPrimaryKey().GetFields()
@@ -170,33 +156,24 @@ func (t *TableIter) GetFullTextFieldValues(k []byte) *map[string][]byte {
 	return &fieldsBytes
 }
 
-/*
-func (t *TableIter) RestoreValByIndex(k []byte) (Value []byte) {
-	ks := util.Bytes(t.iter.Key()).Split() //分解并反转义
-	fieldlen := len(t.table.indexs.GetPrimaryKey().GetFields())
-	ks = ks[len(ks)-fieldlen:]
-	if len(ks) > 1 { //组合主键,分解反转义后再重新拼接
-		for _, k := range ks { //重新拼接
-			Value = append(Value, k...)
-			Value = append(Value, []byte(SPLIT)...)
-		}
-		Value = Value[:len(Value)-1]
-	} else { //单主键
-		Value = ks[0]
-	}
-	return
-}
-*/
 // 通过当前迭代器获取所能得到的所有字段值
+// 主键得到所有的记录字段值，其他索引得到主键值
 func (t *TableIter) GetFieldByte(k, v []byte) (fieldsBytes *map[string][]byte) {
 	//判断索引类型
 	switch t.index.(type) {
 	case FullTextIndex:
 		//全文索引时，value值为空，需要从key中提取主键，末尾就是对应主键的值
 		fieldsBytes = t.GetFullTextFieldValues(k)
-
-	default:
+	case PrimaryKey: //主键得到所有的记录字段值，其他索引得到主键值
 		fieldsBytes = t.table.ParseRecord(v)
+	case NormalIndex:
+		ks := util.Bytes(v).Split() //分解并反转义
+		pkfields := t.table.indexs.GetPrimaryKey().GetFields()
+		//该段代码与GetFullTextFieldValues不同处，GetFullTextFieldValues是从key中提取主键，而NormalIndex是从value中提取主键。
+		fieldsBytes = &map[string][]byte{}
+		for i, k := range ks { //重新拼接
+			(*fieldsBytes)[pkfields[i]] = k
+		}
 	}
 	return fieldsBytes
 }
@@ -227,7 +204,7 @@ func (t *TableIter) GerRecords(esc bool, limit ...int) (r record.Records) {
 			loop++
 		} else {
 			if len(t.iter.Value()) == 0 { //这种情况是全文索引，value值为空，需要从key中提取主键，末尾就是对应主键的值
-				Value = t.RestoreValByIndex(t.iter.Key())
+				Value = t.RestoreValByFullTextIndex(t.iter.Key())
 			} else {
 				Value = t.iter.Value()
 			}
@@ -307,33 +284,45 @@ func (t *TableIter) ForExport(esc bool, export Export) {
 	}
 }
 
-// 提取转换主键值
-// 单主键则返回转换的值，组合主键则返回字符串
-// field用于对组合主键提取其中的部分值
-func (t *TableIter) GetPrimaryKeys(field ...string) (r any) {
-	//是否组合主键
-	isComposite := len(t.table.indexs.GetPrimaryKey().GetFields()) > 1
-	switch isComposite {
-	case true:
-		// 组合主键，返回字符串
-		keys := util.Bytes(t.iter.Key()).Split()
-		if len(field) > 0 {
-			newkeys := ""
-			for _, f := range field {
-				for i, p := range t.table.indexs.GetPrimaryKey().GetFields() {
-					if f == p {
-						newkeys += string(keys[i]) + SPLIT
-					}
-				}
-			}
-			r = newkeys[:len(newkeys)-1]
-		}
-	default:
-		// 单主键，返回转换的值
-		r = util.Bytes(t.iter.Value()).ToAny(t.table.fields[t.table.indexs.GetPrimaryKey().GetFields()[0]])
+// 提取主键值
+// if len(fields) == 0 ，默认是提取主键值，主键也可以是组合主键
+// 单主键则返回原始值，组合主键则返回拼接的字符串
+func (t *TableIter) GetPrimaryKeys(k, v []byte, fields ...string) (r any) {
+	fbs := t.GetFieldByte(k, v)
+	if len(fields) == 0 {
+		fields = t.table.indexs.GetPrimaryKey().GetFields()
 	}
-	return r
+	//提取对应的值
+	val := make(map[string][]byte, len(fields))
+	for _, f := range fields {
+		if v, ok := (*fbs)[f]; ok {
+			val[f] = v
+		}
+	}
+	isComposite := len(fields) > 1
+	if isComposite {
+		r = ""
+		for _, f := range val {
+			r = r.(string) + string(f) + SPLIT
+		}
+	} else {
+		r = util.Bytes((*fbs)[fields[0]]).ToAny(t.table.fields[fields[0]])
+	}
+	return
 }
+
+// 将某字段的所有值转换为map[any]bool
+// if len(fields) == 0 ，默认是提取主键值，否则提取指定字段值
+func (t *TableIter) Map(fields ...string) (data map[any]bool) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	data = make(map[any]bool)
+	for t.iter.Next() {
+		data[t.GetPrimaryKeys(t.iter.Key(), t.iter.Value(), fields...)] = true
+	}
+	return
+}
+
 func (t *TableIter) First() bool {
 	return t.iter.First()
 }
@@ -354,4 +343,20 @@ func (t *TableIter) Key() []byte {
 }
 func (t *TableIter) Value() []byte {
 	return t.iter.Value()
+}
+
+// 判断是否存在指定的主键记录
+func (t *TableIter) Exist() bool {
+	return t.iter.First()
+}
+
+// 统计索引记录数
+func (t *TableIter) Count() int {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	i := 0
+	for t.iter.Next() {
+		i++
+	}
+	return i
 }
