9bdb5608b11baa149be187f1d9077563aa42c4d1
liaoran123 <2065551801@qq.com>
2026-01-13 15:16:02 +0800

    千万级数据库批量添加和千万级搜索验证全部通过。

--- engine/tableiter.go
+++ engine/tableiter.go
@@ -107,6 +107,7 @@ func (t *TableIter) ParseBytes(k, v []byte) *map[string][]byte {
 	return fieldsBytes
 }
 
+// 记录未进行反转义，可能性极低，暂时不处理。
 func (t *TableIter) ParseRecord(fieldsBytes *map[string][]byte) (rd Record) {
 	if fieldsBytes == nil {
 		return nil
