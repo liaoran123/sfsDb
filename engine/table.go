24b7f970bdc65d8b18aa471c4254cf7aa01278c5
liaoran123 <2065551801@qq.com>
2026-01-18 15:27:51 +0800

    反转义

--- engine/table.go
+++ engine/table.go
@@ -396,7 +396,7 @@ func (t *Table) FormatRecord(fieldsBytes *map[string][]byte) (r []byte) {
 
 // Escape()
 // 记录格式：-field1-value1-field2-value2-...-fieldN-valueN-
-// 值包含-fieldname-时，需要转义，即加一个空格- fieldname-。这样可以避免与字段名冲突，反转义时去掉空格。
+// 值包含-fieldname-时，需要转义，即加一个空格- fieldname -。这样可以避免与字段名冲突，反转义时去掉空格。
 func (t *Table) Escape(b []byte) []byte {
 	//b,可能是字段名称，故而也需要转义，即加一个空格- fieldname -。这样可以避免与字段名冲突，反转义时去掉空格。
 	for field := range t.fields {
@@ -407,7 +407,7 @@ func (t *Table) Escape(b []byte) []byte {
 
 // UnEscape()
 // 记录格式：-field1-value1-field2-value2-...-fieldN-valueN-
-// 值包含-fieldname-时，需要转义，即加一个空格- fieldname-。这样可以避免与字段名冲突，反转义时去掉空格。
+// 值包含-fieldname-时，需要转义，即加一个空格- fieldname -。这样可以避免与字段名冲突，反转义时去掉空格。
 func (t *Table) UnEscape(b []byte) []byte {
 	for field := range t.fields {
 		b = bytes.ReplaceAll(b, []byte(" "+field+" "), []byte(field))
