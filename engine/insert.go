61ff61eb3d34070d5ac2be7eaae18bddb9c50c28
liaoran123 <2065551801@qq.com>
2026-02-13 08:36:07 +0800

    不重要

---
+++ engine/insert.go
@@ -0,0 +1,13 @@
+package engine
+
+/* 可有可无功能
+//添加记录流程接口
+type Insert interface {
+	//自动增值主键
+	AutoIncrement() int
+	FieldsToBytes(fields *map[string]any) *map[string][]byte
+	FormatRecord(fieldsBytes *map[string][]byte) []byte
+	GetID() []byte
+	Commit() error
+}
+*/
